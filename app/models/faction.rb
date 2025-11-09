34526d75b111a4521dd31c36fca62dfe676f0b12
Sune Sylvest Nilausen <sunenilausen@gmail.com>
2025-11-09 14:17:19 +0100

    Faction has name and color validation

--- app/models/faction.rb
+++ app/models/faction.rb
@@ -1,2 +1,7 @@
 class Faction < ApplicationRecord
+  COLORS = %w[red blue teal purple yellow green gray lightblue darkgreen brown].freeze
+  NAMES = [ "Gondor", "Rohan", "Dol Amroth", "Fellowship", "Fangorn", "Isengard", "Easterlings", "Harad", "Nazgul", "Mordor" ].freeze
+
+  validates :name, presence: true, uniqueness: true
+  validates :color, presence: true, inclusion: { in: COLORS }
 end

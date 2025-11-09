74dd7643984aa45ef8d5175c29e88bbad67178ba
Sune Sylvest Nilausen <sunenilausen@gmail.com>
2025-11-09 16:57:58 +0100

    Added import players script

---
+++ script/import_players.rb
@@ -0,0 +1,14 @@
+example_players_csv = <<~CSVCSV
+nickname,elo_rating
+PlayerOne,1500
+PlayerTwo,1600
+PlayerThree,1400
+CSVCSV
+
+require 'csv'
+CSV.parse(example_players_csv, headers: true) do |row|
+  Player.create!(
+    nickname: row['nickname'],
+    elo_rating: row['elo_rating'].to_i || 1500
+  )
+end

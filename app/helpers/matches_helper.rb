e08541646f945bbfc51f0d9e1d4ddb9d8bfd3988
Sune Sylvest Nilausen <sunenilausen@gmail.com>
2025-11-09 16:44:27 +0100

    Pretty new match form

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -16,7 +16,7 @@ module MatchesHelper
   end
 
   def avg_elo_rating(appearances)
-    total_elo = appearances.to_a.sum { |appearance| appearance.player.elo_rating.to_i }
+    total_elo = appearances.to_a.sum { |appearance| appearance.elo_rating.to_i }
     (total_elo.to_f / appearances.size).round
   end
 end

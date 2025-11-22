ad5f51a8ab66c70b34ffd9fa46e838c8e767cf28
Sune Sylvest Nilausen <sunenilausen@gmail.com>
2025-11-22 23:50:04 +0100

    Script to export players to csv

--- script/import_players.rb
+++ script/import_players.rb
@@ -13,3 +13,15 @@ CSV.parse(example_players_csv, headers: true) do |row|
     elo_rating_seed: row['elo_rating'].to_i || 1500
   )
 end
+
+def export_players_csv
+  CSV.generate do |csv|
+    csv << [ "nickname", "elo_rating" ]
+    Player.find_each do |player|
+      csv << [ player.nickname, player.elo_rating ]
+    end
+  end
+end
+
+# Example usage:
+# puts export_players_csv

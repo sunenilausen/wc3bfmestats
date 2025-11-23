1f84d51543cbfa6b39cda93f477b56833fb2ac6c
Sune Sylvest Nilausen <sunenilausen@gmail.com>
2025-11-23 04:02:26 +0100

    Underdog stat

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,42 @@
 class HomeController < ApplicationController
   def index
+    @underdog_stats = calculate_underdog_stats
+  end
+
+  private
+
+  def calculate_underdog_stats
+    matches_with_data = Match.includes(appearances: :faction).where.not(good_victory: nil)
+
+    underdog_wins = 0
+    total_matches = 0
+
+    matches_with_data.find_each do |match|
+      good_appearances = match.appearances.select { |a| a.faction&.good? }
+      evil_appearances = match.appearances.select { |a| a.faction && !a.faction.good? }
+
+      good_elos = good_appearances.map(&:elo_rating).compact
+      evil_elos = evil_appearances.map(&:elo_rating).compact
+
+      next if good_elos.empty? || evil_elos.empty?
+
+      good_avg = good_elos.sum.to_f / good_elos.size
+      evil_avg = evil_elos.sum.to_f / evil_elos.size
+
+      next if good_avg == evil_avg
+
+      total_matches += 1
+
+      good_is_underdog = good_avg < evil_avg
+      underdog_won = (good_is_underdog && match.good_victory) || (!good_is_underdog && !match.good_victory)
+
+      underdog_wins += 1 if underdog_won
+    end
+
+    {
+      underdog_wins: underdog_wins,
+      total_matches: total_matches,
+      percentage: total_matches > 0 ? (underdog_wins.to_f / total_matches * 100).round(1) : 0
+    }
   end
 end
